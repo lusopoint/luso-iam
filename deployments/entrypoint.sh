7cb4d2a02db6cd85ac1412345c16e3c683b3fd9e
lee <leedasilva988@gmail.com>
2026-06-04 22:42:39 +0100

    refactor(docker): add more variables to seed-user on entrypoint 'username' and 'displayName' and fix the admin which is boolean

--- deployments/entrypoint.sh
+++ deployments/entrypoint.sh
@@ -4,8 +4,12 @@ set -e
 /iam-server &
 IAM_PID=$!
 
-# wait until db is ready to populate
-until /seed-user -email "${ADMIN_EMAIL}" -password "${ADMIN_PASSWORD}" -admin 1 -name "${ADMIN_NAME}" 2>/dev/null; do
+until /seed-user \
+    -email "${ADMIN_EMAIL}" \
+    -password "${ADMIN_PASSWORD}" \
+    -name "${ADMIN_NAME}" \
+    -username "${ADMIN_USERNAME}" \
+    -admin 2>/dev/null; do
     echo "...waiting for DB to be ready"
     sleep 2
 done
